deebb6ca8e57436163c44c81fb342a6faa9d1925
Radhakant Panda <114462764+Radhakantpanda1@users.noreply.github.com>
2024-06-20 18:12:26 +0530

    tax_percentage_modification

--- 01_basics_week_01/05_basics.py
+++ 01_basics_week_01/05_basics.py
@@ -3,21 +3,26 @@
 user_salary=int(input("Enter your salary- "))
 if(user_salary>=0 and user_salary<=500000):
     income_tax_percentage=0
+    print("Tax to be paid= ", income_tax_percentage)
     print("Amount to be paid =",0)
 elif (user_salary>500000 and user_salary<=1000000):
     income_tax_percentage=10
+    print("Tax to be paid= ", income_tax_percentage)
     tax_to_be_paid=(10/100)*user_salary
     print("Amount to be paid =",tax_to_be_paid)
 elif(user_salary>1000000 and user_salary<=1500000):
     income_tax_percentage=15
+    print("Tax to be paid= ", income_tax_percentage)
     tax_to_be_paid=(15/100)*user_salary
     print("Amount to be paid =",tax_to_be_paid)
 elif(user_salary>1500000 and user_salary<=10000000):
     income_tax_percentage=30
+    print("Tax to be paid= ", income_tax_percentage)
     tax_to_be_paid=(30/100)*user_salary
     print("Amount to be paid =",tax_to_be_paid)
 else:
     income_tax_percentage=35
+    print("Tax to be paid= ", income_tax_percentage)
     tax_to_be_paid=(30/100)*user_salary
     print("Amount to be paid =",tax_to_be_paid)
 
