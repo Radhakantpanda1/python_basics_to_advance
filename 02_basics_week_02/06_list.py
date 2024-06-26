ed64dbaeb8da9ad05e45b35f37cdb6c405c585d1
Radhakant Panda <114462764+Radhakantpanda1@users.noreply.github.com>
2024-06-26 15:57:47 +0530

    list_basics

---
+++ 02_basics_week_02/06_list.py
@@ -0,0 +1,20 @@
+user=['Radhakant Panda',21,True]
+for i in user:
+    print(i)
+l = ["subbbbh" , 356453500,"koigfsafa", True, 4055+8j , [3,5,6,"dfg"],6.7023687]
+print(len(l))
+for i in l:
+    print(i)
+
+for j in  l:
+    print(type(j))
+for j in  l:
+    if(type(j)==list):
+        for k in j:
+            print(k)
+
+l2=list()
+print(type(l2))
+
+for k in range(10):
+    print(k)
