3b18477251a3d4b2fdbab383150f8370231d654e
Radhakant Panda <114462764+Radhakantpanda1@users.noreply.github.com>
2024-07-02 08:44:30 +0530

    and_or on numbers

---
+++ 03_basics_week_03/temp.py
@@ -0,0 +1,11 @@
+print(8 and 4)
+print(4 and 8)
+
+a=10
+b=20
+print( a and b) # 20
+print(b and a) # 10
+# what happens here is precedence is fom left side so a is treated is 1 and b as 0 so 1 and 0 is 0 which is b
+
+print(4 or 8)  # 4
+print( 8 or 4) # 8
